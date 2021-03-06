1af0aefde8377dc2d349f61f3a7e1d7f1482337e
Nazkoro <50379512+Nazkoro@users.noreply.github.com>
2021-03-06 13:16:06 +0300

    feat: add code

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arrayName) {
+  let resultShifr =""
+
+  let arrFilter = arrayName.filter(item => typeof item === 'string')
+  for(let i = 0; i<arrFilter.length;i++){
+    resultShifr += arrFilter[i].trim().substring(0, 1).toUpperCase() 
+    
+  }
+  console.log(resultShifr);
+ let a = resultShifr.split('')
+ let b = a.sort()
+ 
+ return resultShifr.split('').sort().join('')
+ 
 };
